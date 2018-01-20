818769bc75bc8a47a1c3233b058db3a92ca421a3
Shubham Goyal <goyal.choose.resilience@gmail.com>
2018-01-20 21:16:35 +0530

    Edit code and new tests added

--- steps/index.js
+++ steps/index.js
@@ -1,20 +1,34 @@
 function steps(n) {
     var steps_arr = []
     var index = 0
-    for(var i = 1; i <= n; i++) {
-        var steps_str = ''
-        for(var j = 1; j <= i; j++) {
-            steps_str = steps_str + '#'    
+    if (n === 1) {
+        console.log('#')
+    } 
+    else 
+    {
+        for(var i = 1; i <= n; i++) 
+        {
+            var steps_str = ''
+            for(var j = 1; j <= i; j++) 
+            {
+                steps_str = steps_str + '#'    
+            }
+            for(var k = i; k < n ; k++) 
+            {
+                steps_str = steps_str + ' '
+            }
+            steps_arr[index] = steps_str
+            index = index + 1
         }
-        steps_arr[index] = steps_str
-        index = index + 1
+        for(let step of steps_arr) 
+        {
+            console.log(step)
+        }
     }
-    for(let step of steps_arr) {
-        console.log(step)
-    }
+    
 }
 
 
 
-//module.exports = steps;
-steps(4)
+module.exports = steps;
+//steps(2)
