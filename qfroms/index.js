89f64fdef9d4708560c0496b4250f6cef6c6a947
Shubham Goyal <goyal.choose.resilience@gmail.com>
2018-01-22 10:27:33 +0530

    Final code to implement a queue using two stacks

--- qfroms/index.js
+++ qfroms/index.js
@@ -3,8 +3,35 @@
 // *Do not* create an array inside of the 'Queue' class.
 // Queue should implement the methods 'add', 'remove', and 'peek'.
 
-const Stack = require('./stack.js');
+const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.stackOne = new Stack()
+        this.stackTwo = new Stack()
+    }
+
+    add(data) {
+        if (this.stackOne && this.stackTwo) {
+            while (this.stackOne.peek() !== undefined) 
+            {
+                this.stackTwo.add(this.stackOne.remove())
+            }
+            this.stackOne.add(data)
+            while(this.stackTwo.peek() !== undefined) 
+            {
+                this.stackOne.add(this.stackTwo.remove())
+            }
+        } 
+    }
+    
+    peek() {
+        return this.stackOne.peek()
+    }
+
+    remove() {
+        return this.stackOne.remove()
+    }
+}
 
 module.exports = Queue;
