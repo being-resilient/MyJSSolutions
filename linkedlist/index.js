15470cfbe049dfc67f2b4b1241abc35927356b44
Shubham Goyal <goyal.choose.resilience@gmail.com>
2018-01-23 16:01:24 +0530

    Implement code for get node at a given position

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -72,20 +72,56 @@ class LinkedList {
         }
     }
 
-    insertLast(data) {
+    /* insertLast(data) {
         var temp = this.head
         var prev = null
         const node = new Node(data)
-        while (temp) {
-            prev = temp
-            temp = temp.next
+        if (!temp) {
+            temp = node
+            this.head = temp
+        } else {
+            while (temp) {
+                prev = temp
+                temp = temp.next
+            }
+            prev.next = node
         }
-        prev.next = node
+    } */
+
+    insertLast(data) {
+        const node = new Node(data)
+        if (!this.head) {
+            this.head = node
+        } else {
+            var temp = this.head
+            while(temp.next !== null) {
+                temp = temp.next
+            }
+            temp.next = new Node(data)
+        }
+        return this.head
+    }
+
+    getAt(position) {
+        var crawl = 0
+        var temp = this.head
+        if (this.size() < position) {
+            return null
+        } else {
+            while(temp) {
+                if (crawl === position) {
+                    return temp
+                } else {
+                    temp = temp.next
+                    crawl += 1
+                }
+            } 
+        }
     }
 }
 
 module.exports = { Node, LinkedList };
-//const list = new LinkedList();
+const list = new LinkedList();
 
 // list.size()
 //list.insertFirst('e')
@@ -100,6 +136,14 @@ module.exports = { Node, LinkedList };
 //list.insertFirst('j')
 //list.insertFirst('e')
 //list.insertFirst('f')
+console.log(list.insertLast(1));
+console.log(list.insertLast(2));
+console.log(list.insertLast(3));
+console.log(list.insertLast(4));
+console.log(list.getAt(0).data)
+console.log(list.getAt(1).data)
+console.log(list.getAt(2).data)
+console.log(list.getAt(3).data)
 //list.insertFirst('g')
 //list.insertFirst('h')
 //list.insertFirst('i')
@@ -114,4 +158,3 @@ module.exports = { Node, LinkedList };
 // console.log(list.size())
 //console.log(list.getLast())
 // console.log(list.clear())
-// console.log(list.size())
